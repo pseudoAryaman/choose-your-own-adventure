d9b6aa612f60faa73527719e9606c9ed9162ef39
Aryaman <yes.waryaman@gmail.com>
2026-09-22 19:31:47 +0530

    allowed orings split & env turned case sensitive

--- backend/core/config.py
+++ backend/core/config.py
@@ -10,3 +10,12 @@ class Settings(BaseSettings):
     DEBUG: bool = False
     ALLOWED_ORIGIN: str = ""
     OPENAI_API_KEY : str 
+
+    @field_validator("ALLOWED_ORIGINS")
+    def parse_allowed_origins(cls, v:str) -> List[str]:
+        return v.split(",") if v else[]
+
+    class config:
+        env_file = ".env"
+        env_file_encoding = "UTF-8"
+        case_sensitive = True
